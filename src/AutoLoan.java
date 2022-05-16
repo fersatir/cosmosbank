2ec3ea7754aa421cb112c6852504c9fb6e02a0d0
fersatir <fatihersatir@gmail.com>
2022-05-16 21:07:53 +0300

    fe v

--- src/AutoLoan.java
+++ src/AutoLoan.java
@@ -38,6 +38,5 @@ public class AutoLoan implements Credits {
                     kredi();
             }
         }
-
     }
 }
--- src/InfoServices.java
+++ src/InfoServices.java
@@ -9,8 +9,8 @@ import java.util.Set;
 
 
 public class InfoServices {
-    static int numSummary = 1; //fatih log kaydı alırken işlem sırasını 1001'den başlatmak için kullanıldı.
-    public static Map<Integer, InfoVariables> processSummary = new HashMap<>();//fatih log kayıtlarının tutulduğu map
+    static int numSummary = 1; //fatih --> log kaydı alırken işlem sırasını 1001'den başlatmak için kullanıldı.
+    public static Map<Integer, InfoVariables> processSummary = new HashMap<>();//fatih -->log kayıtlarının tutulduğu map
 
     static Map<Integer, ExtractOfAccount> extAcc = new HashMap<>();
     static int num = 1;
@@ -60,7 +60,7 @@ public class InfoServices {
         System.out.println("\n\n\n\n\n----------------------------------------");
         System.out.println("HAVE A GOOD DAY - WE WISH YOU AGAIN");
 
-        System.out.println("\nCOSMOS BANK \nTRANSECTION SUMMARY______________________"); // bu kısım ve aşağıdaki kod bloğu fatih tarafından yapıldı. exit ile birlikte log kayıtlarını göstermek için
+        System.out.println("\nCOSMOS BANK \nTRANSECTION SUMMARY______________________"); // fatih--> bu kısım ve aşağıdaki kod bloğu fatih tarafından yapıldı. exit ile birlikte log kayıtlarını göstermek için
         System.out.printf("%40s", date3);
         System.out.println();
         System.out.printf("\n%-7s %-35s %-14s", islem,  tarih,  process);
@@ -81,7 +81,7 @@ public class InfoServices {
         }
     }
 
-    public static void transactionSummary(String processName, String customerId) {// log alma işlemi yapan method, yapılan işlemin açıklaması ve müşteri id girilecek.
+    public static void transactionSummary(String processName, String customerId) {//fatih--> log alma işlemi yapan method, yapılan işlemin açıklaması ve müşteri id girilecek.
         InfoVariables summary = new InfoVariables();
         summary.setDate();
         summary.setTime();
@@ -93,7 +93,7 @@ public class InfoServices {
 
     }
 
-    public static void transactionSummaryPrint(Map<Integer, InfoVariables> processSummary) {// log kayıtlarını yazdırma methodu
+    public static void transactionSummaryPrint(Map<Integer, InfoVariables> processSummary) {//fatih--> log kayıtlarını yazdırma methodu
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MMM yyyy");
         DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("k:mm:ss");
         Set<Map.Entry<Integer, InfoVariables>> sumary = processSummary.entrySet();
